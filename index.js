7b837dfca1996a03c5ff118fb31c46a3044b6d97
Phil Ershadi <phildoge@gmail.com>
2017-07-26 18:57:06 -0400

    a little reorganizing

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     alert("This is an alert!");
 })
+
+// weather retrieval function, run once
+
+// time incrementing function, place in a set interval
+
+// function that cycles the greeting message, run each 10 seconds
+
+// function that takes the text input and adds it to the greeting message
+
+// function that calls the quote api and print the message, run each minute
