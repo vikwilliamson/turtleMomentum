95b781cf27d41e613abaccee34fef13cf29998cf
Phil Ershadi <phildoge@gmail.com>
2017-07-29 00:34:09 -0400

    add clock, random background

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-    alert("This is an alert!");
+    clockInterval = setInterval(updateTime, 1000);
 })
 
 // weather retrieval function, run once
 
 // time incrementing function, place in a set interval
-
+function updateTime(){
+    date = new Date();
+    hours = date.getHours();
+    minutes = (date.getMinutes()<10 ? '0':'') + date.getMinutes();
+    seconds = (date.getSeconds()<10 ? '0':'') + date.getSeconds();
+    $("#time-block").text(hours + ":" + minutes + ":" + seconds);
+    }
 // function that cycles the greeting message, run each 10 seconds
 
 // function that takes the text input and adds it to the greeting message
