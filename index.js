3914d4b8e0e4402f9586a6a4a760787e3a94122f
Phil Ershadi <phildoge@gmail.com>
2017-07-31 16:58:26 -0400

    added random quote function

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 $(document).ready(function() {
     clockInterval = setInterval(updateTime, 1000);
+    messageInterval = setInterval(updateMessage, 10000);
 })
+
 
 // weather retrieval function, run once
 
+
 // time incrementing function, place in a set interval
+
 function updateTime(){
-    date = new Date();
-    hours = date.getHours();
-    minutes = (date.getMinutes()<10 ? '0':'') + date.getMinutes();
-    seconds = (date.getSeconds()<10 ? '0':'') + date.getSeconds();
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = (date.getMinutes()<10 ? '0':'') + date.getMinutes();
+    var seconds = (date.getSeconds()<10 ? '0':'') + date.getSeconds();
     $("#time-block").text(hours + ":" + minutes + ":" + seconds);
-    }
+}
+
+
 // function that cycles the greeting message, run each 10 seconds
+
+
 
 // function that takes the text input and adds it to the greeting message
 
+
+
 // function that calls the quote api and print the message, run each minute
+
+function updateMessage(){
+    //fade out the current message
+    $("#quote-block").animate({opacity: '0'});
+    //to replace the current message, get the JSON from the quote API
+    var quoteURL = "http://api.icndb.com/jokes/random?firstName=CHUCK&lastName=NORRIS";
+    $.getJSON(quoteURL).done(function(data){
+        //extract the value
+        console.log(data.value.joke);
+        var joke = data.value.joke;
+        //put it into the div
+        setTimeout(function(){$("#quote-block").text(joke);}, 250);
+        //fade the div back in
+        $("#quote-block").animate({opacity: "1.0"});
+        //if the API call fails, at least say something
+    }).fail(function(){
+        console.log("Joke API failed to retrieve a joke.  For shame!");
+        $("#quote-block").text = "Chuck Norris broke our API with nothing but a stern look. Death tolls are in the thousands.";
+    })
+}
